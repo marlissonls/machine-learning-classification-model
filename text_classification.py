bb03915a7f822eae4bcdf779be866c754afbb5a4
Marlisson Silva <marlisson.business@gmail.com>
2023-11-13 18:17:25 -0300

    add create models function

--- text_classification.py
+++ text_classification.py
@@ -89,3 +89,19 @@ def preprocessing_pipeline():
     pipeline = [('tfidf', vectorizer), ('svd', decomposition)]
 
     return pipeline
+
+
+## MODEL SELECTION
+
+N_NEIGHBORS = 4
+CV = 3
+
+def criar_modelos():
+
+    modelo_1 = KNeighborsClassifier(n_neighbors = N_NEIGHBORS)
+    modelo_2 = RandomForestClassifier(random_state = RANDOM_STATE)
+    modelo_3 = LogisticRegressionCV(cv = CV, random_state = RANDOM_STATE)
+
+    modelos = [('KNN', modelo_1), ('RandomForest', modelo_2), ('LogReg', modelo_3)]
+
+    return modelos
