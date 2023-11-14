94680720c8bf5f3df8aef9b7b7d8ec1e270d5aea
Marlisson Silva <marlisson.business@gmail.com>
2023-11-13 21:44:31 -0300

    add distribution and confusion plot functions

--- text_classification.py
+++ text_classification.py
@@ -145,3 +145,42 @@ if __name__ == '__main__':
     resultados = treina_avalia(all_models, pipeline, X_treino, X_teste, y_treino, y_teste)
 
 print('Concluído com sucesso!')
+
+
+## Viewing results
+
+def plot_distribution():
+    _, counts = np.unique(labels, return_counts = True)
+    sns.set_theme(style = 'whitegrid')
+    plt.figure(figsize = (15, 6), dpi = 120)
+    plt.title(('Número de Posts Por Assunto'))
+    sns.barplot(x = assuntos, y = counts)
+    plt.legend([' '.join([f.title(), f'- {c} posts']) for f, c in zip(assuntos, counts)])
+    plt.show()
+
+def plot_confusion(result):
+    print('Relatório de Classificação\n', result[-1]['report'])
+    y_pred = result[-1]['predictions']
+    conf_matrix = confusion_matrix(y_teste, y_pred)
+    _, test_counts = np.unique(y_teste, return_counts = True)
+    conf_matrix_percent = conf_matrix / test_counts.transpose() * 100
+    plt.figure(figsize = (9, 8), dpi = 120)
+    plt.title(result[-1]['model'].upper() + ' resultados')
+    plt.xlabel('Valor Real')
+    plt.ylabel('Previsão do Modelo')
+    ticklabels = [f'r/{sub}' for sub in assuntos]
+    sns.heatmap(data = conf_matrix_percent, xticklabels = ticklabels, yticklabels = ticklabels, annot = True, fmt = '.2f')
+    plt.show()
+
+
+# evaluation
+plot_distribution()
+
+# KNN results
+plot_confusion(resultados[0])
+
+# RandomForest results
+plot_confusion(resultados[1])
+
+# Logistic Regression results
+plot_confusion(resultados[2])
